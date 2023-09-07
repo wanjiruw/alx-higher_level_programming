c27f5e318ef5a99597945829f580be3c5f843f77
wanjiruw <wanjymacharia@gmail.com>
2023-09-07 23:25:57 +0000

    q5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """ brings the variable a from the file and prints its value."""
     import variable_load_5
-    print(variable_load_5.a)
+    print("{}".format(variable_load_5.a))
