da9eb6075b08cfe1263a84e5dc7444e8000b283a
wanjiruw <wanjymacharia@gmail.com>
2023-09-08 00:13:00 +0000

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """prints all the names defined by compiled module hidden_4.pyc"""
     import hidden_4
-    for i in dir(hidden_4):
-        if i;i.startswith('__'):
-            continue
-        print(i)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
