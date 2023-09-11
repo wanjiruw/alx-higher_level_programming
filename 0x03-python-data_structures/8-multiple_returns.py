6a364046c302c7baff9c3d5c4fd7bd0c3341133f
wanjiruw <wanjymacharia@gmail.com>
2023-09-11 20:08:07 +0000

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        character = None
-    return(len(sentence), character[0])
+        return = None
+    return(len(sentence),[0])
