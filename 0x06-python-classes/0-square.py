59bb20dfee7fd0b6164277a3a8a3ba5a3af26366
wanjiruw <wanjymacharia@gmail.com>
2023-09-26 23:31:05 +0000

    mmh

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" python3 -c 'print(__import__("my_module").__doc__)' """
+""" This module contains the definition of and empty Square Class."""
+
 
 class Square:
-
-""" python3 -c 'print(__import__("my_module").MyClass.__doc__)' """
-	pass
+    """ This is an empty Square Class."""
+    pass
 
