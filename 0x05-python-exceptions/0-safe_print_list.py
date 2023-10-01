b335f0410f118d71d8143bc49adfd8b7a6492fed
wanjiruw <wanjymacharia@gmail.com>
2023-10-01 15:52:57 +0100

    understand kwanza

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         n = 0
         for i in range(x):
-            print(my_list[i], end= "")
+            print(my_list[i], end='')
             n += 1
         print()
     except IndexError:
