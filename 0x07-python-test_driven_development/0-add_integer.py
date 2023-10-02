b7ae394b32bb0e46dccc743ef6e4fd3685a74493
wanjiruw <wanjymacharia@gmail.com>
2023-10-02 02:34:37 +0100

    task 1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A functoin that adds integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    A function that adds two integers
+    """
+    if type(a) is not float and type(a) is not int:
+        raise TypeError("a must be an integer")
+    elif type(b) is not float and type(b) is not int:
+        raise TypeError("b must be an integer")
+        return int(a) + int(b)
